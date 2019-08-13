a40fde30628a469ffb7a148a2b5a3138a1a2550c
jlvalport <jlvalport@gmail.com>
2019-08-13 17:01:40 -0600

    feat: quote section in landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import LandingSection from './LandingSection/LandingSection';
 import ServicesSection from './ServicesSection/ServicesSection';
+import QuoteSection from '../../hoc/UI/QuoteSection/QuoteSection';
 
 const LandingPage = () => {
     return (
         <React.Fragment>
             <LandingSection />
             <ServicesSection />
+            <QuoteSection>Hacemos <br />Arte <span>&amp;</span> <br />Ciencia</QuoteSection>
         </React.Fragment>
     );
 }
---
+++ src/hoc/UI/QuoteSection/QuoteSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './QuoteSection.module.scss';
+import triangleVector from '../../../assets/img/graphics/triangle.svg'
+
+const QuoteSection = props => {
+    return (
+        <section className={classes.outerBox}>
+            <img src={triangleVector} alt='' className={classes.triangleVector} />
+            <img src={triangleVector} alt='' className={classes.triangleVector} />
+            <div className={classes.innerBox}>
+                <p>{props.children}</p>
+            </div>
+        </section>
+    );
+}
+
+export default QuoteSection
