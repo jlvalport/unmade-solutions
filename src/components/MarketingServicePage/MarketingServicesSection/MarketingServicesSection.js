a6f2d21c8e007618fd2b1f80fbb22c6c2f94712b
jlvalport <jlvalport@gmail.com>
2019-11-07 13:10:41 -0600

    feat: marketing page services section desktop view

--- src/components/MarketingServicePage/MarketingServicesSection/MarketingServicesSection.js
+++ src/components/MarketingServicePage/MarketingServicesSection/MarketingServicesSection.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import SubSectionTitle from '../../UI/SubSectionTitle/SubSectionTitle';
-import MarketingService from  './MarketingService/MarketingService';
+import MarketingService from './MarketingService/MarketingService';
 import socialMediaManagementIcon from '../../../assets/img/graphics/icons/team.svg'
 import socialMediaMarketingIcon from '../../../assets/img/graphics/icons/social-media.svg'
 import emailMarketingIcon from '../../../assets/img/graphics/icons/email.svg'
+import classes from './MarketingServicesSection.module.scss';
 
 const MarketingServicesSection = () => {
     return (
         <React.Fragment>
             <SubSectionTitle>Trabajamos estas áreas de tu empresa:</SubSectionTitle>
-            <MarketingService title='Marketing de redes sociales' icon={socialMediaManagementIcon} />
-            <MarketingService title='Manejo de redes sociales' icon={socialMediaMarketingIcon} />
-            <MarketingService title='Marketing de email' icon={emailMarketingIcon} />
+            <div className={classes.servicesInfoBox}>
+                <MarketingService title='Marketing de redes sociales' icon={socialMediaManagementIcon} />
+                <MarketingService title='Manejo de redes sociales' icon={socialMediaMarketingIcon} />
+                <MarketingService title='Marketing de email' icon={emailMarketingIcon} />
+            </div>
         </React.Fragment>
     );
 };
