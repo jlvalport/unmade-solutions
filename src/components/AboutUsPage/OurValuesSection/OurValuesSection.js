cc7abf9422453211a60ba67aa20704ed49809dba
jlvalport <jlvalport@gmail.com>
2019-11-09 07:37:59 -0600

    feat: about us page values section desktop view

--- src/components/AboutUsPage/OurValuesSection/OurValuesSection.js
+++ src/components/AboutUsPage/OurValuesSection/OurValuesSection.js
@@ -9,18 +9,21 @@ import accountabilityIcon from '../../../assets/img/graphics/icons/tick.svg';
 import passionIcon from '../../../assets/img/graphics/icons/heart-outline.svg';
 import diversityIcon from '../../../assets/img/graphics/icons/group-of-men.svg';
 import qualityIcon from '../../../assets/img/graphics/icons/quality.svg';
+import classes from './OurValuesSection.module.scss';
 
 const OurValuesSection = () => {
     return (
         <React.Fragment>
             <SectionTitle tag='h2' bgColor='orange'>Nuestros Valores</SectionTitle>
-            <Value title='Liderazgo' infoText='El coraje de moldear un futuro mejor.' icon={leadershipIcon} />
-            <Value title='Colaboración' infoText='Aprovechar el genio colectivo.' icon={colaborationIcon} />
-            <Value title='Integridad' infoText='Se real.' icon={integrityIcon} />
-            <Value title='Responsabilidad' infoText='Si va a ser, depende de mí.' icon={accountabilityIcon} />
-            <Value title='Pasión' infoText='Comprometidos en corazón y mente.' icon={passionIcon} />
-            <Value title='Diversidad' infoText='Incluyendo siempre a todos.' icon={diversityIcon} />
-            <Value title='Calidad' infoText='Lo que hacemos, lo hacemos bien.' icon={qualityIcon} />
+            <div className={classes.valuesBox}>
+                <Value title='Liderazgo' infoText='El coraje de moldear un futuro mejor.' icon={leadershipIcon} />
+                <Value title='Colaboración' infoText='Aprovechar el genio colectivo.' icon={colaborationIcon} />
+                <Value title='Integridad' infoText='Se real.' icon={integrityIcon} />
+                <Value title='Responsabilidad' infoText='Si va a ser, depende de mí.' icon={accountabilityIcon} />
+                <Value title='Pasión' infoText='Comprometidos en corazón y mente.' icon={passionIcon} />
+                <Value title='Diversidad' infoText='Incluyendo siempre a todos.' icon={diversityIcon} />
+                <Value title='Calidad' infoText='Lo que hacemos, lo hacemos bien.' icon={qualityIcon} />
+            </div>
         </React.Fragment>
     );
 };
