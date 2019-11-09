94ba8639721726ff3bf882c8c87ed6525f2ee938
jlvalport <jlvalport@gmail.com>
2019-11-09 15:53:54 -0600

    feat: contactUsForm connected to serverless email delivery

--- src/components/UI/ContactForm/ContactForm.js
+++ src/components/UI/ContactForm/ContactForm.js
@@ -16,38 +16,38 @@ const CForm = props => {
         <Form className={classes.contactForm}>
             <div className={classes.inputBox}>
                 <label className={touched.name && errors.name ? [classes.label, classes.labelError].join(' ') : classes.label} >Mi nombre es
-                <ErrorMessage name='name' component='p' className={classes.error}/>
-                <Field type='text' name='name' placeholder='Tu nombre' autoComplete='name' className={ bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
+                <ErrorMessage name='name' component='p' className={classes.error} />
+                    <Field type='text' name='name' placeholder='Tu nombre' autoComplete='name' className={bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
                 </label>
             </div>
             <div className={classes.inputBox}>
                 <label className={touched.email && errors.email ? [classes.label, classes.labelError].join(' ') : classes.label} >Contáctame a
-                <ErrorMessage name='email' component='p' className={classes.error}/>
-                <Field type='email' name='email' placeholder='tu@correo.com' autoComplete='email' className={ bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
+                <ErrorMessage name='email' component='p' className={classes.error} />
+                    <Field type='email' name='email' placeholder='tu@correo.com' autoComplete='email' className={bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
                 </label>
             </div>
             <div className={classes.inputBox}>
                 <label className={touched.phone && errors.phone ? [classes.label, classes.labelError].join(' ') : classes.label} >Mi número de teléfono es
-                <ErrorMessage name='phone' component='p' className={classes.error}/>
-                <Field type='phone' name='phone' placeholder='Tu número de teléfono' autoComplete='tel' className={ bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
+                <ErrorMessage name='phone' component='p' className={classes.error} />
+                    <Field type='phone' name='phone' placeholder='Tu número de teléfono' autoComplete='tel' className={bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
                 </label>
             </div>
             <div className={classes.inputBox}>
                 <label className={touched.company && errors.company ? [classes.label, classes.labelError].join(' ') : classes.label} >Trabajo en
-                <ErrorMessage name='company' component='p' className={classes.error}/>
-                <Field type='text' name='company' placeholder='Tu compañía' autoComplete='organization' className={ bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
+                <ErrorMessage name='company' component='p' className={classes.error} />
+                    <Field type='text' name='company' placeholder='Tu compañía' autoComplete='organization' className={bgColor ? [classes.inputElement, classes[bgColor]].join(' ') : classes.inputElement} />
                 </label>
             </div>
             <div className={classes.inputBox}>
                 <label className={touched.message && errors.message ? [classes.label, classes.labelError].join(' ') : classes.label} >Mensaje
-                <ErrorMessage name='message' component='p' className={classes.error}/>
-                <Field component='textarea' name='message' placeholder='Empieza a escribir...' autoComplete='off' className={ bgColor ? [classes.textAreaElement, classes[bgColor]].join(' ') : classes.textAreaElement} />
+                <ErrorMessage name='message' component='p' className={classes.error} />
+                    <Field component='textarea' name='message' placeholder='Empieza a escribir...' autoComplete='off' className={bgColor ? [classes.textAreaElement, classes[bgColor]].join(' ') : classes.textAreaElement} />
                 </label>
             </div>
             <button type='submit' className={classes.sendBtn} disabled={isSubmitting}>Enviar</button>
         </Form>
-        );
-    };
+    );
+};
 
 const ContactForm = withFormik({
     mapPropsToValues({ name, email, phone, company, message }) {
@@ -67,8 +67,23 @@ const ContactForm = withFormik({
         message: Yup.string().required('Mensaje requerido')
     }),
     handleSubmit(values) {
-        console.log(values);
+        // This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
+        const endpoint = 'https://r0vu7fp7zb.execute-api.us-west-2.amazonaws.com/stage-one';
         
+        // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
+        var lambdaRequest = new Request(endpoint, {
+            method: 'POST',
+            // Quick note: 'no-cors' mode is for development on localhost only!
+            mode: 'no-cors',
+            body: JSON.stringify(values)
+        });
+
+        // Call the Fetch API to make our request
+        fetch(lambdaRequest)
+            // This is where you can handle errors. This is just an example, so we won't cover that.
+            .then(response => console.log(response))
+            .catch(err => console.log(err));
+
     }
 })(CForm)
 
